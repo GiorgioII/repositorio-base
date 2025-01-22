b4b06d376e71e68baf2e789c20d65e02781ea36d
Jorge Boullosa Trejo <boullosa65boullosa9@gmail.com>
2025-01-22 11:12:50 +0100

    Página web

---
+++ mi pagina web jorge/PoemasdelMundo.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="estilos.css">
+
+</head>
+<body>
+
+<center><p>POEMAS DEL MUNDO</P>
+            <p>*</P>
+            <p>Encontraré el horizonte en tus ojos;<p> 
+            <p>en tu camino sobre las olas del mar</p>
+            <p>suspiras aleteos de intrépidas aves</p>
+            <p>entrelazando tus dedos,</p>
+            <p>transformas en plumas tu ser;</p> 
+            <p>abres tus ojos al cielo</p>
+            <p>agitando tus alas al viento,</P>
+            <p>te elevas y te vas</p>
+            <p>Encontraré el horizonte en tus ojos,</p>
+            <p>otro día... Te dará por conversar,</p> 
+            <p>mientras deshojas margaritas</p>
+            <p>susurrándole al viento</p> 
+            <p>instantes de verdad</p>
+    
+    <p>BULHOSA / 2025</P>
+     
+        <a href="prueba.php">Regresar</a>
+
+    <center>
+
+    
+
+    
+
+</body>
+</html>
---
+++ mi pagina web jorge/prueba.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">     
+    <title>BouArt</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+
+<body>
+    <center>
+    <div>
+    
+    <center><p>UN ESPACIO PARA DISFRUTAR</p>
+    <a id="ir" href="https://www.instagram.com/">ir</a><center>
+
+    <header>
+        
+        <ul id="Encabezado">
+            <li><a href="inicio.php"> INICIO </a></li>
+            <li><a href="biografia.php"> BIOGRAFÍA </a></li>
+            <li><a href="contacto.php"> CONTACTO </a></li>
+            <li><a href="galeria.php"> GALERÍA </a></li>
+        </ul>
+
+    </header>
+
+    <aside>
+
+        <ul id="BarraLateral">
+            <li><a href="PoemasdelMundo.php"> POEMAS </a></li>
+            <li><a href="ensayos.php"> ENSAYOS </a></li>
+            <li><a href="cuentos.php"> CUENTOS </a></li>
+            <li><a href="relatos.php"> RELATOS </a></li>
+            <li><a href="novelas.php"> NOVELAS</a></li>
+        </ul>
+    </aside>
+
+    <section>
+         <center>   
+         <img src="campos roturados.jpg" alt="No se ha cargado la imagen">
+         <a href="obra.php">Ver detalles</a>
+         <center>
+         
+
+
+
+    </section>
+
+</div>
+<center>
+
+
+
+
+    
+        
+           <
+</body>
+
+
+</html>
